c8a8aa75704e267c42e4826b9ac37aa9088bb117
Imanaliev Yernur <imanalievernur5002@gmail.com>
2025-12-05 21:01:39 +0500

    empty

---
+++ endTermDL1/src/main/java/com/example/endTermDL1/EndTermDl1Application.java
@@ -0,0 +1,13 @@
+package com.example.endTermDL1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndTermDl1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndTermDl1Application.class, args);
+	}
+
+}
---
+++ endTermDL1/src/test/java/com/example/endTermDL1/EndTermDl1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.endTermDL1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndTermDl1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
